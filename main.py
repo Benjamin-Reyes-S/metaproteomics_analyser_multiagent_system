aefbffc2c9e671da32aac918a19ee6fd53a56737
Reyes <benjamin.reyes@isas.de>
2026-07-21 16:22:57 +0200

    Planner lower class + node (agent call and message response with structured format output)

--- main.py
+++ main.py
@@ -3,28 +3,23 @@ from langgraph.graph import StateGraph, START, END
 from agents import statistical_analyser, study_planer, study_validator, outcome_auditor
 
 
-
-def study_planner_node(state) -> TypeDict:
-    result= study_planer.invoke(
-        message
-    )
-
-
 # orchestration state decides which node runs nex
 
 builder = StateGraph(MetaproteomicsAnylisisState)
 
 # architecture of the graph 
 builder.add_node("study design", study_planner_node)
-builder.add_node("analyze and plot", statistical_analyser)
-builder.add_node("studdy validator", study_validator)
-builder.add_node("evaluate output", outcome_auditor)
+#builder.add_node("analyze and plot", statistical_analyser)
+#builder.add_node("studdy validator", study_validator)
+#builder.add_node("evaluate output", outcome_auditor)
 
 builder.add_edge(START, "Study design")
-builder.add_edge("study design", "evaluate output")
-builder.add_edge("evaluate output", "analyze and plot")
-builder.add_edge("analyze and plot", "studdy validator")
-builder.add_edge("studdy validator", END)
+builder.add_edge("Study design", END)
+
+#builder.add_edge("study design", "evaluate output")
+#builder.add_edge("evaluate output", "analyze and plot")
+#builder.add_edge("analyze and plot", "studdy validator")
+#builder.add_edge("studdy validator", END)
 
 graph = builder.compile()
 
